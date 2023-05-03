a53cbcecfe2d22b183aea2c3a777a2e79d0b963a
Rostislav Rabiec <rosta.rabiec@icloud.com>
2023-05-03 23:22:51 +0200

    Added currency option. The code has been arranged.

--- main.py
+++ main.py
@@ -9,18 +9,29 @@ window.resizable(True, True)
 amount_label = Label(text="Investovaná částka:", font=("Arial", 20))
 amount_label.grid(row=0, column=0, padx=10, pady=10)
 
+user_amount_input = Entry(width=10, font=("Arial", 20), justify=RIGHT)
+user_amount_input.insert(0, "")
+user_amount_input.grid(row=0, column=1, padx=10, pady=25)
+
+currency_drop_down = StringVar(window)
+currency_drop_down.set("CZK")
+currency_drop_down_set = OptionMenu(window, currency_drop_down, "CZK", "EUR", "USD")
+currency_drop_down_set.grid(row=0, column=2, padx=10, pady=25)
+
 interest_rate_label = Label(text="Roční úroková sazba:", font=("Arial", 20))
-interest_rate_label.grid(row=1, column=0, padx=10, pady=10)
+interest_rate_label.grid(row=1, column=0, padx=10, pady=(10, 0))
 
 num_of_years_label = Label(text="Počet let:", font=("Arial", 20))
 num_of_years_label.grid(row=2, column=0, padx=10, pady=10)
 
-result_label = Label(text="Konečná částka:", font=("Arial", 20))
-result_label.grid(row=3, column=0, padx=10, pady=10)
+count_button = Button(window, text="Spočítej", font=("Arial", 20))
+count_button.grid(row=3, column=1, padx=10, pady=10)
 
-user_amount_input = Entry(width=10, font=("Arial", 20), justify=RIGHT)
-user_amount_input.insert(0, "")
-user_amount_input.grid(row=0, column=1, padx=10, pady=25)
+result_label = Label(text="Konečná částka:", font=("Arial", 20))
+result_label.grid(row=4, column=0, padx=10, pady=10)
+
+final_amount_label = Label(text="0", fg="black", font=("Arial", 20))
+final_amount_label.grid(row=4, column=1)
 
 user_interest_rate_input = Entry(width=10, font=("Arial", 20), justify=RIGHT)
 user_interest_rate_input.insert(0, "")
@@ -29,8 +40,5 @@ user_interest_rate_input.grid(row=1, column=1, padx=10, pady=25)
 user_years_input = Entry(width=10, font=("Arial", 20), justify=RIGHT)
 user_years_input.insert(0, "")
 user_years_input.grid(row=2, column=1, padx=10, pady=25)
-
-final_amount_label = Label(text="0", fg="black", font=("Arial", 20))
-final_amount_label.grid(row=3, column=1)
 
 window.mainloop()
