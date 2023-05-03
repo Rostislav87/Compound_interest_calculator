16331f4343bd91f72b2f86f721cd0e508ea0f7cc
Rostislav Rabiec <rosta.rabiec@icloud.com>
2023-05-03 22:36:14 +0200

    Added entru labels for user inputs and label for final amount

--- main.py
+++ main.py
@@ -18,4 +18,19 @@ num_of_years_label.grid(row=2, column=0, padx=10, pady=10)
 result_label = Label(text="Konečná částka:", font=("Arial", 20))
 result_label.grid(row=3, column=0, padx=10, pady=10)
 
+user_amount_input = Entry(width=10, font=("Arial", 20), justify=RIGHT)
+user_amount_input.insert(0, "")
+user_amount_input.grid(row=0, column=1, padx=10, pady=25)
+
+user_interest_rate_input = Entry(width=10, font=("Arial", 20), justify=RIGHT)
+user_interest_rate_input.insert(0, "")
+user_interest_rate_input.grid(row=1, column=1, padx=10, pady=25)
+
+user_years_input = Entry(width=10, font=("Arial", 20), justify=RIGHT)
+user_years_input.insert(0, "")
+user_years_input.grid(row=2, column=1, padx=10, pady=25)
+
+final_amount_label = Label(text="0", fg="black", font=("Arial", 20))
+final_amount_label.grid(row=3, column=1)
+
 window.mainloop()
