326b7bad727288b00acf17d29dbee49e6c218c81
Rostislav Rabiec <rosta.rabiec@icloud.com>
2023-05-04 20:38:02 +0200

    The code has been arranged

--- main.py
+++ main.py
@@ -21,8 +21,16 @@ currency_drop_down_set.grid(row=0, column=2, padx=10, pady=25)
 interest_rate_label = Label(text="Roční úroková sazba:", font=("Arial", 20))
 interest_rate_label.grid(row=1, column=0, padx=10, pady=(10, 0))
 
+user_interest_rate_input = Entry(width=10, font=("Arial", 20), justify=RIGHT)
+user_interest_rate_input.insert(0, "")
+user_interest_rate_input.grid(row=1, column=1, padx=10, pady=25)
+
 num_of_years_label = Label(text="Počet let:", font=("Arial", 20))
 num_of_years_label.grid(row=2, column=0, padx=10, pady=10)
+
+user_years_input = Entry(width=10, font=("Arial", 20), justify=RIGHT)
+user_years_input.insert(0, "")
+user_years_input.grid(row=2, column=1, padx=10, pady=25)
 
 count_button = Button(window, text="Spočítej", font=("Arial", 20))
 count_button.grid(row=3, column=1, padx=10, pady=10)
@@ -32,13 +40,5 @@ result_label.grid(row=4, column=0, padx=10, pady=10)
 
 final_amount_label = Label(text="0", fg="black", font=("Arial", 20))
 final_amount_label.grid(row=4, column=1)
-
-user_interest_rate_input = Entry(width=10, font=("Arial", 20), justify=RIGHT)
-user_interest_rate_input.insert(0, "")
-user_interest_rate_input.grid(row=1, column=1, padx=10, pady=25)
-
-user_years_input = Entry(width=10, font=("Arial", 20), justify=RIGHT)
-user_years_input.insert(0, "")
-user_years_input.grid(row=2, column=1, padx=10, pady=25)
 
 window.mainloop()
